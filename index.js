cb052eb4a74c3a38df4e14183b37a1fd8a084a54
Bryan Veloso <bryan@avalonstar.com>
2015-04-21 14:09:35 -0700

    Try using winston.

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
+// Logging.
+var winston = require('winston');
+
 // Configure express.
 app.use(express.static(__dirname + '/'));
 
 // Instantiate a server.
 var server = http.createServer(app).listen(port);
-console.log('http server listening on %d', port);
+winston.debug('HTTP server listening on %d.', port);
 
 // Instantiate socket.io.
 var io = require('socket.io')(server);
-console.log('socket.io server created');
+winston.debug('Socket.io server created.');
 
 io.on('connection', function(socket) {
-  console.log('socket.io connection open');
+  winston.log('Socket.io connection opened.');
 
   // Events.
   // Subscription sending/receiving.
